cc16ee8e24dbfd5f3ace62d9e46bf0f1f4929f54
Ivan Pugliese <ivanpugliese02@gmail.com>
2022-12-02 13:07:07 -0300

    Tercer commit

--- js/main.js
+++ js/main.js
@@ -17,3 +17,50 @@
     menu.classList.remove("menu-active");
   });
 })(document);
+
+/********** Modo oscuro **********/
+function darkTheme(btn) {
+  const themeBtn = document.querySelector(btn);
+  const selectors1 = document.querySelectorAll("[data-dark]");
+  const selectors2 = document.querySelectorAll("[data-dark-features]");
+  const selectors3 = document.querySelectorAll("[data-dark-shadow]");
+
+  let moon = "🌙";
+  let sun = "☀️";
+
+  const lightMode = () => {
+    selectors1.forEach((el) => el.classList.remove("dark-theme"));
+    selectors2.forEach((el) => el.classList.remove("dark-theme-features"));
+    selectors3.forEach((el) => el.classList.remove("dark-theme-shadow"));
+    themeBtn.textContent = moon;
+    localStorage.setItem("theme", "light");
+  };
+  const darkMode = () => {
+    selectors1.forEach((el) => el.classList.add("dark-theme"));
+    selectors2.forEach((el) => el.classList.add("dark-theme-features"));
+    selectors3.forEach((el) => el.classList.add("dark-theme-shadow"));
+    themeBtn.textContent = sun;
+    localStorage.setItem("theme", "dark");
+  };
+
+  document.addEventListener("click", (e) => {
+    if (e.target.matches(btn)) {
+      if (themeBtn.textContent === moon) {
+        darkMode();
+      } else {
+        lightMode();
+      }
+    }
+  });
+
+  document.addEventListener("DOMContentLoaded", (e) => {
+    if (localStorage.getItem("theme") === null)
+      localStorage.setItem("theme", "light");
+
+    if (localStorage.getItem("theme") === "light") lightMode();
+
+    if (localStorage.getItem("theme") === "dark") darkMode();
+  });
+}
+
+darkTheme(".dark-theme-btn");
